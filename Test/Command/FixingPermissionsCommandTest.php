a12a856f491b20e29f4b5700c3f67616528aee7b
Jerry Anselmi <jerry.anselmi@gmail.com>
2017-03-21 22:19:33 -0400

    Commit de respaldo, inicio de las pruebas unitarias

---
+++ Test/Command/FixingPermissionsCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Output\StreamOutput;
+
+class FixingPermissionsCommandTest extends \PHPUnit_Framework_TestCase
+{
+    // protected static $application;
+    // public static function setUpBeforeClass()
+    // {
+    //     $kernel = new \AppKernelTest("test", true);
+    //     $kernel->boot();
+    //     static::$application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+    // }
+}
